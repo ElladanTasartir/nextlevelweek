6b6e73e202da2ffdea6669da00329d48cf69981a
Erick Malta <erickmalta100@gmail.com>
2020-06-05 22:57:20 -0300

    Desafio do Native Picker Select completo!

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   ImageBackground,
@@ -9,17 +9,57 @@ import {
   KeyboardAvoidingView,
   Platform,
 } from "react-native";
+import RNPickerSelect from "react-native-picker-select";
 import { Feather as Icon } from "@expo/vector-icons";
 //ImageBackground é como se fosse uma view que aceita uma imagem de fundo
 //Instalado a biblioteca react-navigation junto com algumas dependências da doc dela
 //Instalado a dependência @react-navigation/stack
 import { RectButton } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+
+interface IBGEUFResponse {
+  sigla: string;
+}
+
+interface IBGECityResponse {
+  nome: string;
+}
 
 const Home = () => {
   const navigation = useNavigation(); //possui uma função navigate que realiza a navegação entre as rotas
   const [uf, setUf] = useState("");
   const [city, setCity] = useState("");
+  const [ufs, setUfs] = useState<string[]>([]);
+  const [cities, setCities] = useState<string[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<IBGEUFResponse[]>(
+        "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+      )
+      .then((response) => {
+        const ufInitials = response.data.map((uf) => uf.sigla);
+
+        setUfs(ufInitials);
+        setCity('');
+      });
+  }, []);
+
+  useEffect(() => {
+    if (uf === "") {
+      return;
+    }
+    axios
+      .get<IBGECityResponse[]>(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`
+      )
+      .then((response) => {
+        const cityNames = response.data.map((city) => city.nome);
+
+        setCities(cityNames);
+      });
+  }, [uf]);
 
   function handleNavigateToPoints() {
     navigation.navigate("Points", { uf, city });
@@ -50,14 +90,14 @@ const Home = () => {
         </View>
 
         <View style={styles.footer}>
-          <TextInput
+          {/* <TextInput
             style={styles.input}
             placeholder="Digite a UF"
             value={uf}
             onChangeText={setUf}
             maxLength={2}
             autoCapitalize="characters"
-            autoCorrect={false}//não tenta corrigir o texto digitado
+            autoCorrect={false} //não tenta corrigir o texto digitado
           />
           <TextInput
             style={styles.input}
@@ -65,6 +105,25 @@ const Home = () => {
             value={city}
             onChangeText={setCity}
             autoCorrect={false}
+          /> */}
+
+          <RNPickerSelect
+            onValueChange={(value) => setUf(value)}
+            items={ufs.map((item) => {
+              return {
+                label: item,
+                value: item,
+              };
+            })}
+          />
+          <RNPickerSelect
+            onValueChange={(value) => setCity(value)}
+            items={cities.map((item) => {
+              return {
+                label: item,
+                value: item,
+              };
+            })}
           />
 
           <RectButton style={styles.button} onPress={handleNavigateToPoints}>
